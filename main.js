eeaf46132a31a672bd1153d036e2effac7f55834
greghxc <greg@hacksy.io>
2019-03-21 01:28:54 -0700

    update import syntax for default

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// where you esm-based application truly begins
-import {default as sqr} from './lib/simple_square.js'
+// where you esm-enabled application truly begins
+import simple_square from './lib/simple_square.js'
 import {pi, cube} from './lib/math_util.js'
 
-console.log(sqr(4))
+console.log(simple_square(4))
 // 16
 console.log(cube(2))
 // 8
